56c5d2ff3a1e3d392a2bc3fa3645a70ba5983250
Irina Illustrova <illustrova@gmail.com>
2021-06-19 02:46:14 +0300

    uograde webpack

--- next.config.js
+++ next.config.js
@@ -3,6 +3,17 @@ const withMDX = require("@next/mdx")({
 });
 
 module.exports = withMDX({
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  },
   pageExtensions: ["js", "jsx", "mdx"],
   trailingSlash: true,
+   future: {
+    webpack5: true,
+  },
 });
