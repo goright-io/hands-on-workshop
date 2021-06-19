10180bf6eabc8b5f6320dd15f646a96956541b23
Irina Illustrova <illustrova@gmail.com>
2021-06-19 10:06:12 +0300

    Updates

---
+++ new/_next/static/chunks/pages/index copy-a8f816e35a08c18dae34.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9209],{88015:function(){},9309:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/index copy",function(){return u(88015)}])}},function(n){var _,u=(_=9309,n(n.s=_));_N_E=u}]);
